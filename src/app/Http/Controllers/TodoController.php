d8899e995a7e789c82223912c8658dff78f33e46
ayuka <basketball110512@gmail.com>
2024-03-30 19:08:08 +0900

    1-5完了

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
 use Illuminate\Http\Request;
 
@@ -13,10 +14,10 @@ class TodoController extends Controller
         return view('index', compact('todos'));
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
-        $todo = $request->only('content');
+        $todo = $request->only(['content']);
         Todo::create($todo);
-        return redirect('/');
+        return redirect('/')->with('message', 'Todoを作成しました');
     }
 }

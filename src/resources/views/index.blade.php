c3abd45a1d125a9e7663cdcb81272411ab159f9a
ayuka <basketball110512@gmail.com>
2024-04-01 05:08:25 +0900

    2-3完了

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,6 +5,8 @@
 @endsection
 
 @section('content')
+
+<!-- メッセージ表示機能 -->
 <div class="todo__alert">
     @if(session('message'))
     <div class="todo__alert--success">
@@ -21,20 +23,54 @@
     </div>
     @endif
 </div>
+
+<!-- use操作可能箇所 -->
 <div class="todo__content">
+
+    <!-- 新規作成 -->
+    <div class="section__title">
+        <h2>新規作成</h2>
+    </div>
     <form class="create-form" method="post" action="/todos">
         @csrf
         <div class="create-form__item">
-            <input class="create-form__item-input" type="text" name="content">
+            <input class="create-form__item-input" type="text" name="content" value="{{ old('content') }}">
+            <select class="create-form__item-select">
+                <option value="">カテゴリ</option>
+        </select>
+
         </div>
         <div class="create-form__button">
             <button class="create-form__button-submit" type="submit">作成</button>
         </div>
     </form>
+
+    <!-- 検索 -->
+    <div class="section__title">
+        <h2>Todo検索</h2>
+    </div>
+    <form class="search-form" method="post" action="/todos">
+        @csrf
+        <div class="search-form__item">
+            <input class="search-form__item-input" type="text" name="content" value="{{ old('content') }}">
+            <select class="search-form__item-select">
+                <option value="">カテゴリ</option>
+            </select>
+
+        </div>
+        <div class="search-form__button">
+            <button class="search-form__button-submit" type="submit">作成</button>
+        </div>
+    </form>
+
+    <!-- todoテーブル表示 -->
     <div class="todo-table">
         <table class="todo-table__inner">
             <tr class="todo-table__row">
-                <th class="todo-table__header">Todo</th>
+                <th class="todo-table__header">
+                    <span class="todo-table__header-span">Todo</span>
+                    <span class="todo-table__header-span">カテゴリ</span>
+                </th>
             </tr>
             @foreach ($todos as $todo) 
             <tr class="todo-table__row">
@@ -45,6 +81,11 @@
                         <div class="update-form__item">
                             <input class="update-form__item-input" type="text" name="content" value="{{ $todo['content'] }}">
                             <input type="hidden" name="id" value="{{ $todo['id'] }}">
+                        </div>
+                        <div class="update-form__item">
+                            <p class="update-form__item-p">
+                                Category 1
+                            </p>
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
